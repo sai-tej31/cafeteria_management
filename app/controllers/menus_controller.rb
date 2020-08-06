38d1af1533afa1197e7feb78506bbc64cc28ef9d
sai-tej31 <saitejareddy31.com>
2020-08-06 16:47:24 +0530

    update UI for menuudnuds

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -24,24 +24,7 @@ class MenusController < ApplicationController
     menu = Menu.find(id)
     menu.active_menu =active_menu
     menu.save!
-    if menu.active_menu == false
-      flag = 1
-    else
-      flag = 0
-    end
-    Menu.all.each do |menu|
-      if menu.active_menu==true
-        flag+=1
-      end
-    end
-    if flag != 1
-      flash[:error] = "Select Only One Menu as Active Menu"
-      redirect_to new_menu_path
-    else
-      if menu.active_menu == true or flag == 1
-        redirect_to menus_path
-      end
-    end
+    redirect_to new_menu_path
   end
   def destroy
     ensure_owner_logged_in
